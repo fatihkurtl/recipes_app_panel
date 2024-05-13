11ff61bf2a9e2226b3ec8c2ab3324ae07ffa373c
fatihkurtl <kurtf061@gmail.com>
2024-05-13 03:40:48 +0300

    chore: Update RecipesCard component in admin home view and user views

--- src/layouts/global/navbar/NavbarLayout.vue
+++ src/layouts/global/navbar/NavbarLayout.vue
@@ -39,15 +39,15 @@ const logOut = () => {
                             aria-current="page">Home</RouterLink>
                     </li>
                     <li>
-                        <RouterLink to="/users"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                            Users</RouterLink>
-                    </li>
-                    <li>
                         <RouterLink to="/recipes"
                             class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                             Recipes
                         </RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/users"
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                            Users</RouterLink>
                     </li>
                     <li>
                         <RouterLink to="/contact"
